dc9d76565b19eee3f07dc9f3494afa4ab69b38d1
Mohamed Anees M K <138788670+mohamedanees03@users.noreply.github.com>
2024-10-06 11:57:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        StringBuffer sb=new StringBuffer("anees");
+        StringBuffer sb=new StringBuffer("Anees");
         sb.append("Shifa");
         sb.insert(2, " Java");
         sb.deleteCharAt(2);
@@ -12,14 +12,3 @@ public class Main
         System.out.println(sb.capacity());
     }
 }
-
-
-/*
-22:16:15	UPDATE employee SET hourly_pay=15.36 WHERE employee_id=6	
-Error Code: 1175. You are using safe update mode and you tried to update a table without
- a WHERE that uses a KEY column. 
-  To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.	
-  0.015 sec
-
-
- */
